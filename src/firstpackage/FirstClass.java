6f41677010cd3af024b4af9858da193b94e0b7b6
sahan-wq <67201333+sahan-wq@users.noreply.github.com>
2020-06-21 15:46:13 +0200

    Update FirstClass.java
    artik cozum ariyoruz

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,11 +5,11 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
-<<<<<<< HEAD
-		System.out.println("ucuncu commit from local repository");
-=======
+
+		System.out.println("we want to second conflict");
+
 		System.out.println("conflict yapmak istiyoruz mu?");
->>>>>>> refs/remotes/origin/master
+
 		
 		System.out.println("first commit from remote repository");
 
